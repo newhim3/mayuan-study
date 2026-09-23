f33f56794126b6a23fb37a5d2bf25bd5c5cc6d36
程文韬 <dhjdjd13@outlook.com>
2026-09-23 08:43:38 +0800

    Add progress backup and preserve update data

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "mayuan-study-v12";
-const ASSETS = ["./", "./index.html", "./styles.css?v=12", "./app.js?v=12", "./manifest.webmanifest", "./favicon.png", "./assets/icon-192.png", "./assets/icon-512.png", "./data/questions.json?v=12"];
+const CACHE = "mayuan-study-v13";
+const ASSETS = ["./", "./index.html", "./styles.css?v=13", "./app.js?v=13", "./manifest.webmanifest", "./favicon.png", "./assets/icon-192.png", "./assets/icon-512.png", "./data/questions.json?v=13"];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(ASSETS)));
@@ -19,3 +19,4 @@ self.addEventListener("fetch", (event) => {
     return response;
   })));
 });
+
